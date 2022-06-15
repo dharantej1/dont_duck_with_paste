425b7580835fc46a8e4dd4780654c5bef864adfa
Pusthakala Dharan Tej <57841685+dharantej1@users.noreply.github.com>
2022-06-15 22:07:31 +0530

    Add files via upload

---
+++ main_java.py
@@ -0,0 +1,28 @@
+import pyautogui
+import time
+# import pyperclip as pc
+
+# text = pc.paste()
+text=open("copy_text.txt")
+print("Paste the copied text in 'copy_text.txt' file")
+Enter=input("Then Press Enter button here and move to target Page Text Area within 6 seconds")
+time.sleep(6)
+count=0
+for each_line in text:
+    each_line=each_line.strip()
+    if "{" in each_line:
+        count+=1
+        pyautogui.typewrite(each_line)
+        pyautogui.press("Enter")
+        #time.sleep(2)
+    elif "}" in each_line and count > 0:
+        each_line.replace("}","")
+        count-=1
+        pyautogui.press("Down")
+        pyautogui.press("Enter")
+        #time.sleep(2)
+    else:
+        pyautogui.typewrite(each_line)
+        pyautogui.press("Enter")
+        #time.sleep(2)
+# print(count)
